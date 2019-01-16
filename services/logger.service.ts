2c0caeb4a61215af5fa02f1658fd6ce7483dd9b1
Лабутин Евгений Геннадьевич <elabutin@stream.ru>
2019-01-16 12:26:32 +0300

    update logger service

--- services/logger.service.ts
+++ services/logger.service.ts
@@ -5,10 +5,8 @@ import { LogLevels } from "../models/enums/log-levels.enum";
 
 export class LoggerService {
 
-    public readonly events: HTMLElement = document.createElement("div");
-
+    public readonly events: EventTarget = document.createElement("div");
     public logLevel: LogLevels = LogLevels.ERROR;
-    public loginUrl: string | null = null;
 
     public log(message: string, logLevel: LogLevels = LogLevels.INFO) {
         if (logLevel <= this.logLevel) {
@@ -33,11 +31,6 @@ export class LoggerService {
 
     protected processError(message: string, error?: Error, logLevel: LogLevels = LogLevels.ERROR): void {
 
-        if (error && error instanceof NetError && error.status === 401 && this.loginUrl) {
-            window.location.href = this.loginUrl;
-            return;
-        }
-
         const clientError: ClientError = new ClientError();
 
         if (error) {
@@ -46,12 +39,15 @@ export class LoggerService {
             clientError.message = "Ошибка в " + message;
         }
 
+        // error serialize
         clientError.error = JSON.stringify(error, Object.getOwnPropertyNames(error));
 
+        // body
         if ((error && error instanceof NetError) || error instanceof BackError) {
             clientError.errorBody = error.body;
         }
 
+        // navigator serialize
         const varNavigator: { [key: string]: object } = {};
         for (const i in navigator) {
             varNavigator[i] = (navigator as any)[i];
@@ -60,6 +56,7 @@ export class LoggerService {
         delete varNavigator.mimeTypes;
         clientError.browser = JSON.stringify(varNavigator);
 
+        this.events.dispatchEvent(new CustomEvent("error", { detail: { text: JSON.stringify(clientError) } }));
         this.sendClientError(clientError);
     }
 
