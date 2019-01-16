2209789897dd57ddb0ae8522ab45d9f27436472a
Лабутин Евгений Геннадьевич <elabutin@stream.ru>
2019-01-16 11:32:57 +0300

    update logger service

--- models/view-models/client-error.vm.ts
+++ models/view-models/client-error.vm.ts
@@ -1,9 +1,9 @@
 export class ClientError {
     public message: string | null = null;
+    public browser: string | null = null;
+    public browserHash: string | null = null;
     public error: string | null = null;
     public errorHash: string | null = null;
     public errorBody: string | null = null;
-    public browser: string | null = null;
-    public browserHash: string | null = null;
     public count: number = 1;
 }
--- services/logger.service.ts
+++ services/logger.service.ts
@@ -13,25 +13,26 @@ export class LoggerService {
     public log(message: string, logLevel: LogLevels = LogLevels.INFO) {
         if (logLevel <= this.logLevel) {
             console.log(message);
-            this.showModalWithError(message, void 0, logLevel);
+            this.processError(message, void 0, logLevel);
         }
     }
 
     public warning(message: string, logLevel: LogLevels = LogLevels.WARN) {
         if (logLevel <= this.logLevel) {
             console.warn(message);
-            this.showModalWithError(message, void 0, logLevel);
+            this.processError(message, void 0, logLevel);
         }
     }
 
     public error(message: string, error?: Error, logLevel: LogLevels = LogLevels.ERROR) {
         if (logLevel <= this.logLevel) {
             console.error(message, error);
-            this.showModalWithError(message, error, logLevel);
+            this.processError(message, error, logLevel);
         }
     }
 
-    private showModalWithError(message: string, error?: Error, logLevel: LogLevels = LogLevels.ERROR): void {
+    protected processError(message: string, error?: Error, logLevel: LogLevels = LogLevels.ERROR): void {
+
         if (error && error instanceof NetError && error.status === 401 && this.loginUrl) {
             window.location.href = this.loginUrl;
             return;
@@ -40,16 +41,12 @@ export class LoggerService {
         const clientError: ClientError = new ClientError();
 
         if (error) {
-            clientError.message =
-                "Ошибка в " + message + ": " + error.name + " " + error.message;
+            clientError.message = "Ошибка в " + message + ": " + error.name + " " + error.message;
         } else {
             clientError.message = "Ошибка в " + message;
         }
 
-        clientError.error = JSON.stringify(
-            error,
-            Object.getOwnPropertyNames(error)
-        );
+        clientError.error = JSON.stringify(error, Object.getOwnPropertyNames(error));
 
         if ((error && error instanceof NetError) || error instanceof BackError) {
             clientError.errorBody = error.body;
@@ -63,19 +60,11 @@ export class LoggerService {
         delete varNavigator.mimeTypes;
         clientError.browser = JSON.stringify(varNavigator);
 
-        // let errorType: string = "error";
-        // if (error instanceof NetError && error.status === 503) {
-        //     errorType = "updateserver";
-        // }
-
-        // if (environment.production) {
-        //     this.events.dispatchEvent(
-        //         new CustomEvent(errorType, {
-        //             detail: { text: JSON.stringify(clientError) }
-        //         })
-        //     );
-        // } else {
-        //     alertify.error(clientError.message);
-        // }
+        this.sendClientError(clientError);
     }
+
+    protected sendClientError(clientError: ClientError): void {
+        // todo: overide and process client error, example send to server or show modal with error
+    }
+
 }
