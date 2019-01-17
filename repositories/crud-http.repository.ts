1d88e9e65c310592807069c208ae162e60aa377a
Лабутин Евгений Геннадьевич <elabutin@stream.ru>
2019-01-17 10:45:42 +0300

    fix crud http repository

--- repositories/crud-http.repository.ts
+++ repositories/crud-http.repository.ts
@@ -12,31 +12,31 @@ export abstract class CrudHttpRepository<T1 extends Serializable> extends HttpRe
 
     public async getById(id: string | number, ...keys: (string | number)[]): Promise<T1> {
         let url = this.apiRoot;
-        keys.forEach((key) => url = url.replace(/\{.+\}/, String(key)));
+        keys.forEach((key) => url = url.replace(/\{.+\}/g, String(key)));
         return await this.customRequestAsT("GET", `${url}/${id}`, void 0, this.modelConstructor);
     }
 
     public async getAll(...keys: (string | number)[]): Promise<T1[]> {
         let url = this.apiRoot;
-        keys.forEach((key) => url = url.replace(/\{.+\}/, String(key)));
+        keys.forEach((key) => url = url.replace(/\{.+\}/g, String(key)));
         return await this.customRequestAsArrayT("GET", `${url}/`, void 0, [this.modelConstructor]);
     }
 
     public async create(value: T1, ...keys: (string | number)[]): Promise<T1> {
         let url = this.apiRoot;
-        keys.forEach((key) => url = url.replace(/\{.+\}/, String(key)));
+        keys.forEach((key) => url = url.replace(/\{.+\}/g, String(key)));
         return await this.customRequestAsT("POST", `${url}/`, value, this.modelConstructor);
     }
 
     public async update(id: string | number, value: T1, ...keys: (string | number)[]): Promise<void> {
         let url = this.apiRoot;
-        keys.forEach((key) => url = url.replace(/\{.+\}/, String(key)));
+        keys.forEach((key) => url = url.replace(/\{.+\}/g, String(key)));
         return await this.customRequest("PUT", `${url}/${id}`, value, void 0);
     }
 
     public async delete(id: string | number, ...keys: (string | number)[]): Promise<void> {
         let url = this.apiRoot;
-        keys.forEach((key) => url = url.replace(/\{.+\}/, String(key)));
+        keys.forEach((key) => url = url.replace(/\{.+\}/g, String(key)));
         return await this.customRequest("DELETE", `${url}/${id}`, void 0, void 0);
     }
 
