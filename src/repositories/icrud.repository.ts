b7d2d4eab962c38658f7c0e9b1a2decb11c6b40c
Evgeniy Labutin <labeg@mail.ru>
2020-04-11 19:24:56 +0000

    npm publish

--- src/repositories/icrud.repository.ts
+++ src/repositories/icrud.repository.ts
@@ -5,18 +5,18 @@ import { PagedList } from "../models/view-models/paged-list.vm";
 
 export interface ICrudRepository<T1 extends Serializable> {
 
-    getById(id: string | number): Promise<T1>;
+    getById: (id: string | number) => Promise<T1>;
 
-    getAll(): Promise<T1[]>;
+    getAll: () => Promise<T1[]>;
 
-    create(value: T1): Promise<T1>;
+    create: (value: T1) => Promise<T1>;
 
-    update(id: string | number, value: T1): Promise<void>;
+    update: (id: string | number, value: T1) => Promise<void>;
 
-    delete(id: string | number): Promise<void>;
+    delete: (id: string | number) => Promise<void>;
 
-    getGraphById(id: string | number, graphQuery: IGraphQuery): Promise<T1>;
+    getGraphById: (id: string | number, graphQuery: IGraphQuery) => Promise<T1>;
 
-    getPaged(pageListQuery: PageListQuery): Promise<PagedList<T1>>;
+    getPaged: (pageListQuery: PageListQuery) => Promise<PagedList<T1>>;
 
 }
